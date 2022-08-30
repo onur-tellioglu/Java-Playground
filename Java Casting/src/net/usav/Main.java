5d67478192086046965e9a77603c459518257d61
Onur Tellioğlu <94357256+onur-tellioglu@users.noreply.github.com>
2022-08-30 18:22:23 +0300

    Casting methods

---
+++ Java Casting/src/net/usav/Main.java
@@ -0,0 +1,11 @@
+package net.usav;
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 10;
+        float number2 = 20.5f;
+        float sum = (float) number1 + number2;
+
+        System.out.println("Sum: " + sum);
+    }
+}
